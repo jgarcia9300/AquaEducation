d2bde93e41a9c7e6ca33b28474a98a5630f25b4a
Juan Miguel Rojas Noriega <juan.rojas@pragma.com.co>
2024-11-01 15:04:59 -0500

    Se agregan variables de entorno de forma segura

--- firebase.config.js
+++ firebase.config.js
@@ -1,26 +1,19 @@
 import { getAuth } from "firebase/auth"; // Firebase v9
 import { getFirestore } from "firebase/firestore";
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyATfbVK2A6Uae_LRF3uRh7TruPfd5u7ASM",
-  authDomain: "test-f88d7.firebaseapp.com",
-  projectId: "test-f88d7",
-  storageBucket: "test-f88d7.appspot.com",
-  messagingSenderId: "958126925992",
-  appId: "1:958126925992:web:7db41507da46c95e8aba9d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app); // Se crea la instancia de autenticación
-
-//Initialize Firestore and create a reference to the service
-
 export const db = getFirestore(app); // Se crea la instancia de Firestore
