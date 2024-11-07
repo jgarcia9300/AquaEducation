f2ad64941eae83c49f80a7bc6bf75e386b1d3e77
jgarcia9300 <juan.m.garcia@correounivalle.edu.co>
2024-11-07 14:14:07 -0500

    sombras agregados

--- src/pages/escasez-agua/lights/Lights.jsx
+++ src/pages/escasez-agua/lights/Lights.jsx
@@ -1,11 +1,37 @@
+import { DirectionalLightHelper } from "three";
+import { OrthographicCamera, useHelper } from "@react-three/drei";
+import { useRef } from "react";
+
+// const Lights = () => {
+//   return (
+//     <>
+//       <ambientLight intensity={1.5} />
+//       <directionalLight position={[10, 10, 0]} intensity={2} 
+//       castShadow />
+//     </>
+//   );
+// };
+
 const Lights = () => {
+  const lightRef = useRef();
+  useHelper(lightRef, DirectionalLightHelper, 1);
+
   return (
-    <>
-      <ambientLight intensity={1.5} />
-      <directionalLight position={[10, 10, 0]} intensity={2} 
-      castShadow />
-    </>
+
+    <directionalLight
+
+      castShadow
+      position={[6, 30, 26]}
+      shadow-mapSize={[1024, 1024]}
+      color={"#fff7de"}
+      intensity={5}
+    >
+
+      <OrthographicCamera attach="shadow-camera" args={[-10, 10, 10, -10]} />
+    </directionalLight>
+
   );
 };
+
 
 export default Lights;
